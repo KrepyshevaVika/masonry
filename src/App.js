74dbd71b04b8565a845f7273c2caa2ab30303020
KrepyshevaVika <27vita03@mail.ru>
2018-10-08 22:13:17 +0300

    masonry

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'masonry-layout';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <React.Fragment>
+      <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": ".grid-sizer", "gutter": 10, "percentPosition": true }'>
+      <div class="grid-sizer"></div>
+        <div class="grid-item">Static bannerLorem eu pariatur nulla enim duis sunt cupidatat amet reprehenderit. Cillum amet minim dolore laboris excepteur labore veniam ex aliquip adipisicing excepteur labore nostrud. Reprehenderit nisi commodo cillum nostrud sit deserunt. Nulla eiusmod consectetur occaecat anim tempor officia reprehenderit sit in esse esse sint. Nostrud est deserunt proident est ipsum aliquip cillum ut laborum Lorem exercitation ea ea.
+
+Consequat velit do sint aute laborum aliquip anim pariatur nulla est. Anim aliqua velit velit dolore. Enim ipsum magna dolor dolor et est proident ut exercitation. Fugiat Lorem velit ea proident aute magna minim incididunt amet ad nulla nulla anim deserunt. Ipsum proident ipsum deserunt pariatur mollit.
+
+Excepteur ut officia officia laboris ad fugiat sint ipsum consequat adipisicing officia culpa. Laborum incididunt eiusmod culpa magna laborum laboris anim commodo. Quis nisi labore proident cupidatat laboris. Pariatur ullamco officia irure nisi ex et et ut sint aliquip excepteur et. Non qui esse adipisicing esse enim incididunt ipsum. Irure enim ullamco minim culpa eu nisi exercitation.</div>
+        <div class="grid-item">fghdhIn dolor aliqua est nisi qui magna sint amet in elit et velit occaecat. Cupidatat ullamco dolor et aliqua magna ex tempor officia. Magna laboris est ut pariatur culpa excepteur nulla fugiat eiusmod dolore mollit. Laboris duis deserunt laboris in voluptate reprehenderit exercitation tempor nisi minim ea nisi eiusmod. Ad excepteur non dolore consectetur occaecat dolore tempor nostrud aliquip nostrud duis. Amet tempor laborum excepteur ut cupidatat ea exercitation ut Lorem.
+
+Et enim Lorem ut aliquip sunt ullamco ad reprehenderit est exercitation ipsum laborum non velit. Tempor fugiat Lorem aliquip consectetur excepteur culpa ut ex est id dolore. Aliqua aliquip culpa labore aliqua magna proident qui ex proident qui incididunt dolore anim.</div>
+        <div class="grid-item">Veniam aute officia enim voluptate dolore nisi. Occaecat qui mollit ut eiusmod do ea. Veniam in sunt irure cupidatat deserunt.
+
+Nisi id sunt nulla incididunt. Reprehenderit exercitation et ex dolore commodo deserunt. Reprehenderit nisi eiusmod voluptate quis adipisicing.
+
+Sunt sit eiusmod aliquip proident est nostrud exercitation aliquip commodo commodo. Officia mollit ullamco sunt minim sint Lorem reprehenderit. Dolor incididunt non eu aliquip. Magna pariatur enim officia aliquip. Nulla laborum nostrud velit aute quis aliqua laboris deserunt esse velit in nisi minim. Ut cillum eiusmod aliqua laborum cupidatat.
+
+Tempor laboris nulla irure reprehenderit ut cillum commodo sint ex. Reprehenderit nulla elit exercitation non. Consectetur amet ad enim exercitation eiusmod proident Lorem do. Pariatur laboris dolore aute aute voluptate tempor aliquip sint ut eiusmod excepteur adipisicing ullamco do.
+
+Adipisicing magna elit amet nostrud ut tempor sint sunt anim ea. Labore aute incididunt velit non occaecat do velit esse cillum sit. Veniam irure culpa ea aliqua consectetur consectetur cupidatat aliquip incididunt aute amet duis ut nulla. Incididunt aute nisi anim officia reprehenderit anim in culpa labore ea. Pariatur aute culpa anim sint laborum sit sunt laboris eiusmod nulla amet nulla.</div>
       </div>
+      </React.Fragment>
     );
   }
 }
